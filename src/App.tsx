ba29a0ab60e123964e7b1687e9ed9da1d62cb525
TJamaly <64003319+DevJamaly@users.noreply.github.com>
2026-08-12 10:26:25 +0400

    Add code for calculaitng stats from packing list (derived)

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -146,10 +146,29 @@ function Item({ item, onDeleteItem, onToggleItem }: ItemProps) {
     </li>
   );
 }
-function Stats() {
+function Stats({ items }: { items: Item[] }) {
+  if (!items.length)
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </footer>
+    );
+
+  const numItems = items.length;
+  const numPackedItems = items.reduce(
+    (acc, item) => (item.packed ? acc + 1 : acc),
+    0,
+  );
+  const packedPercentage = Math.round((numPackedItems / numItems) * 100) || 0;
+
   return (
     <footer className="stats">
-      <em>💼You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {packedPercentage >= 100
+          ? `You got everything! Ready to go ✈️`
+          : `💼You have ${numItems} items on your list, and you already packed
+        ${numPackedItems} (${packedPercentage}%)`}
+      </em>
     </footer>
   );
 }
