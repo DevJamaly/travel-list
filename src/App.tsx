ab49775dc86ea1005212cd3a3c1375ed99bfd34c
TJamaly <64003319+DevJamaly@users.noreply.github.com>
2026-08-12 12:07:41 +0400

    add selection element for sorting and implement sorting functionality

--- src/App.tsx
+++ src/App.tsx
@@ -108,10 +108,20 @@ interface PackingListProps {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }: PackingListProps) {
+  const [sortBy, setSortby] = useState('input');
+
+  let sortedItems = items;
+  if (sortBy === 'description')
+    sortedItems = items.toSorted((a, b) =>
+      a.description.localeCompare(b.description),
+    );
+  if (sortBy === 'packed')
+    sortedItems = items.toSorted((a, b) => Number(b.packed) - Number(a.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map(item => (
+        {sortedItems.map(item => (
           <Item
             item={item}
             key={item.id}
@@ -120,6 +130,13 @@ function PackingList({ items, onDeleteItem, onToggleItem }: PackingListProps) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={e => setSortby(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
